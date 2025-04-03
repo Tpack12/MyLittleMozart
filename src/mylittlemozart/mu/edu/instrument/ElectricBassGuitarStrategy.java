abc56a0392f2ecfd300752babfb1a326892953f8
Jacob Stirnemann <jacobstirnemann@Jacobs-MacBook-Pro.local>
2025-04-03 16:54:09 -0500

    Created Instrument Strategy and three different instrument strategies

--- src/mylittlemozart/mu/edu/instrument/ElectricBassGuitarStrategy.java
+++ src/mylittlemozart/mu/edu/instrument/ElectricBassGuitarStrategy.java
@@ -1,5 +1,10 @@
 package mylittlemozart.mu.edu.instrument;
 
-public class ElectricBassGuitarStrategy {
-
+public class ElectricBassGuitarStrategy implements InstrumentStrategy {
+    @Override
+    public void applyInstrument(Track track, int channel) {
+        //Set the instrument to Electric Bass Guitar (MIDI instrument 33)
+        track.setInstrument(33);
+        System.out.println("Applying Electric Bass Guitar (MIDI 33) to track on channel " + channel);
+    }
 }

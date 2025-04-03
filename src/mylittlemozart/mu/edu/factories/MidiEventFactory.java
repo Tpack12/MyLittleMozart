b412fa3ca762df080e3012359e9bda4d0f5c7d67
Tanner Pack <tannerpack@Tanners-MacBook-Air.local>
2025-04-03 17:33:03 -0500

    Finished the classes in mylittlemozard.mu.edu.factories. Now on to debugging.

--- src/mylittlemozart/mu/edu/factories/MidiEventFactory.java
+++ src/mylittlemozart/mu/edu/factories/MidiEventFactory.java
@@ -1,5 +1,33 @@
 package mylittlemozart.mu.edu.factories;
 
-public class MidiEventFactory {
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
 
+/**
+ * Interface for creating MIDI note-on and note-off events.
+ */
+public interface MidiEventFactory {
+
+    /**
+     * Creates a MIDI Note On event.
+     *
+     * @param tick     The time in ticks when the note starts
+     * @param note     The pitch value
+     * @param velocity The velocity (volume)
+     * @param channel  The MIDI channel
+     * @return MidiEvent representing the Note On
+     * @throws InvalidMidiDataException if MIDI data is invalid
+     */
+    MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException;
+
+    /**
+     * Creates a MIDI Note Off event.
+     *
+     * @param tick    The time in ticks when the note ends
+     * @param note    The pitch value
+     * @param channel The MIDI channel
+     * @return MidiEvent representing the Note Off
+     * @throws InvalidMidiDataException if MIDI data is invalid
+     */
+    MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException;
 }
--- src/mylittlemozart/mu/edu/factories/StaccatoMidiEventFactoryAbstract.java
+++ src/mylittlemozart/mu/edu/factories/StaccatoMidiEventFactoryAbstract.java
@@ -1,5 +1,17 @@
 package mylittlemozart.mu.edu.factories;
 
-public class StaccatoMidiEventFactoryAbstract {
+/**
+ * Abstract factory for creating a StaccatoMidiEventFactory.
+ */
+public class StaccatoMidiEventFactoryAbstract implements MidiEventFactoryAbstract {
 
+    /**
+     * Returns an instance of StaccatoMidiEventFactory.
+     *
+     * @return a new StaccatoMidiEventFactory
+     */
+    @Override
+    public MidiEventFactory createFactory() {
+        return new StaccatoMidiEventFactory();
+    }
 }
